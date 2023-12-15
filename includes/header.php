edef9582cc3c44213ddfcc1c9bb7c89b0874b216
Sanzida Khan <96487335+miubat@users.noreply.github.com>
2023-12-15 23:53:34 +0600

    include

---
+++ includes/header.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Travel Agency Management System For Next Adventure Tourism</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+
+
+<?php if($_SESSION['login'])
+{?>
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
+			<li class="prnt"><a href="profile.php">My Profile</a></li>
+				<li class="prnt"><a href="change-password.php">Change Password</a></li>
+			<li class="prnt"><a href="tour-history.php">My Tour History</a></li>
+			<li class="prnt"><a href="issuetickets.php">Issue Tickets</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol">Welcome :</li>				
+			<li class="sig"><?php echo htmlentities($_SESSION['login']);?></li> 
+			<li class="sigi"><a href="logout.php" >/ Logout</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div><?php } else {?>
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.php"><i class="fa fa-home"></i></a></li>
+				<li class="hm"><a href="admin/index.php">Admin Login</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol"></li>				
+			<li class="sig"><a href="#"  >Sign Up</a></li> 
+			<li class="sigi"><a href="#"  >/ Sign In</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<?php }?>
+<!--- /top-header ---->
+<!--- header ---->
+<div class="header">
+	<div class="container">
+		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
+			<a href="index.php">Next <span>Adventure Tourism</span></a>	
+		</div>
+	
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /header ---->
+<!--- footer-btm ---->
+<div class="footer-btm wow fadeInLeft animated" data-wow-delay=".5s">
+	<div >
+	<div>
+			<nav >
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div >
+					<nav >
+						<ul >
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+								<li><a href="#">Tour Packages</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<?php if($_SESSION['login'])
+{?>
+								<li>Need Help?<a href="#" data-toggle="modal" data-target="#myModal3"> / Write Us </a>  </li>
+								<?php } else { ?>
+								
+								<?php } ?>
+								<div class="clearfix"></div>
+
+						</ul>
+					</nav>
+				</div><!-- /.navbar-collapse -->	
+			</nav>
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
